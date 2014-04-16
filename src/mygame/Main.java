7ff3659d0086176b6105bd1cd554cee337ebc2f9
Spencer Smith <rsmitty1025@gmail.com>
2014-04-15 21:06:07 -0400

    Shit can now be blown up. Hit spacebar when a box is in the crosshairs. It doesn't actually delete yet, I wanna put in a check for that.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -3,9 +3,13 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -48,13 +52,13 @@ public class Main extends SimpleApplication {
       
       }
     
-    private void SetupUI(String directory){
+    public void SetupUI(String directory){
       
       //Reset cam as UI is reset.
-      cam.setLocation(new Vector3f(0,0,10));
-      
+      //cam.setLocation(new Vector3f(0,0,10));
+              
       //Speed up camera
-      flyCam.setMoveSpeed(3);
+      flyCam.setMoveSpeed(10);
       
       MakeCrosshairs();
       
@@ -84,7 +88,7 @@ public class Main extends SimpleApplication {
       }
     }
     
-    private void MakeABox(String filename, int x, int y, int z){
+    public void MakeABox(String filename, int x, int y, int z){
         Box box = new Box(1,1,1);
         Geometry boxGeo = new Geometry("Box", box);
         boxGeo.setLocalTranslation(new Vector3f(x,y,z));
@@ -95,18 +99,19 @@ public class Main extends SimpleApplication {
         shootables.attachChild(boxGeo);
     }
     
-    private void MakeALabel(String filename, int x, int y, int z){
+    public void MakeALabel(String filename, int x, int y, int z){
         float moveX = (float) x - 0.5f;
         float moveZ = (float) z + 1.02f;
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
         BitmapText helloText = new BitmapText(guiFont, false);
         helloText.setSize(0.3f);
+        helloText.setName("label-"+filename);
         helloText.setText(filename);
         helloText.setLocalTranslation(moveX, 2, moveZ);
         rootNode.attachChild(helloText);
     }
     
-    private void MakeCrosshairs(){
+    public void MakeCrosshairs(){
       guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
       BitmapText cross = new BitmapText(guiFont, false);
       cross.setSize(guiFont.getCharSet().getRenderedSize() * 2);
@@ -115,12 +120,70 @@ public class Main extends SimpleApplication {
       guiNode.attachChild(cross);
     }
     
+    public void BlowShitUp(String boxName, Vector3f collisionLocation){
+        /** Explosion effect. Uses Texture from jme3-test-data library! */ 
+        ParticleEmitter debris = CreateDebris(collisionLocation);
+        ParticleEmitter fire = CreateFire(collisionLocation);
+        
+        rootNode.attachChild(debris);
+        rootNode.attachChild(fire);
+        
+        debris.emitAllParticles();
+        fire.emitAllParticles();
+        
+        shootables.detachChildNamed(boxName);
+        rootNode.detachChildNamed("label-"+boxName);
+        //rootNode.detachChildNamed("Debris");
+        //rootNode.detachChildNamed("Fire");
+    }
+    
+    public ParticleEmitter CreateDebris(Vector3f location){
+        ParticleEmitter debrisEffect = new ParticleEmitter("Debris", ParticleMesh.Type.Triangle, 10);
+        Material debrisMat = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        debrisMat.setTexture("Texture", assetManager.loadTexture("Effects/Explosion/Debris.png"));
+        debrisEffect.setMaterial(debrisMat);
+        debrisEffect.setImagesX(3); debrisEffect.setImagesY(3); // 3x3 texture animation
+        debrisEffect.setRotateSpeed(4);
+        debrisEffect.setSelectRandomImage(true);
+        debrisEffect.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 8, 0));
+        debrisEffect.setStartColor(new ColorRGBA(1f, 1f, 1f, 1f));
+        debrisEffect.setGravity(0f,6f,0f);
+        debrisEffect.getParticleInfluencer().setVelocityVariation(.60f);
+        debrisEffect.setLocalTranslation(location);
+        debrisEffect.setParticlesPerSec(0);
+        return debrisEffect;
+    }
+    
+    public ParticleEmitter CreateFire(Vector3f location){
+        /** Uses Texture from jme3-test-data library! */
+        ParticleEmitter fireEffect = new ParticleEmitter("Fire", ParticleMesh.Type.Triangle, 30);
+        Material fireMat = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        fireMat.setTexture("Texture", assetManager.loadTexture("Effects/Explosion/flame.png"));
+        fireEffect.setMaterial(fireMat);
+        fireEffect.setImagesX(2); fireEffect.setImagesY(2); // 2x2 texture animation
+        fireEffect.setEndColor( new ColorRGBA(1f, 0f, 0f, 1f) );   // red
+        fireEffect.setStartColor( new ColorRGBA(1f, 1f, 0f, 0.5f) ); // yellow
+        fireEffect.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 2, 0));
+        fireEffect.setStartSize(1.5f);
+        fireEffect.setEndSize(0.1f);
+        fireEffect.setGravity(0f,0f,0f);
+        fireEffect.setLowLife(0.5f);
+        fireEffect.setHighLife(3f);
+        fireEffect.getParticleInfluencer().setVelocityVariation(0.3f);
+        fireEffect.setLocalTranslation(location);
+        fireEffect.setParticlesPerSec(0);
+        return fireEffect;
+
+    }
+    
     /** Declaring the "Shoot" action and mapping to its triggers. */
     public void initKeys() {
       inputManager.addMapping("Shoot", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
       inputManager.addMapping("Back", new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
+      inputManager.addMapping("Delete", new KeyTrigger(KeyInput.KEY_SPACE));
       inputManager.addListener(shootListener, "Shoot");
       inputManager.addListener(shootListener, "Back");
+      inputManager.addListener(shootListener, "Delete");
     }
     /** Defining the "Shoot" action: Determine what was hit and how to respond. */
     private ActionListener shootListener = new ActionListener() {
@@ -152,8 +215,27 @@ public class Main extends SimpleApplication {
             SetupUI(currentPath);
             }
         }
+        
+        if (name.equals("Delete") && !keyPressed) {
+
+          CollisionResults results = new CollisionResults();
+          Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+          shootables.collideWith(ray, results);
+
+          if (results.size() > 0) {
+            CollisionResult closest = results.getClosestCollision();
+            String closestName = closest.getGeometry().getName();
+            String closestPath = fileHash.get(closest.getGeometry().getName());
+            Vector3f collisionLocation = closest.getGeometry().getLocalTranslation();
+            BlowShitUp(closestName,collisionLocation);
+            currentPath = closestPath;
+              
+          }
+        }
+        
       }
     };
+    
     
     
     
